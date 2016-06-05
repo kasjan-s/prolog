b5bf4582c478148e7ef760bc23ab716843f52a79
Kasjan Siwek <kasjan.siwek@gmail.com>
2016-06-05 19:34:01 +0200

    Wykonuja sie wszystkie obliczenia

---
+++ zadanie.pl
@@ -0,0 +1,133 @@
+:- ensure_loaded(library(lists)).
+
+:- op(700, xfx, <>).
+
+<>(X, Y) :- X =\= Y.
+
+init_vars([], []).
+init_vars([H|T], [(H,0)|NT]) :-
+  init_vars(T,NT).
+  
+map(nil).
+map(node(L, _, _, R)) :- map(L), map(R).
+lookup_map(node(_, K, V, _), Key, Value) :-
+  K =:= Key,
+  !,
+  Value is V.
+lookup_map(node(L, K, _, _), Key, Value) :-
+  Key < K,
+  !,
+  lookup_map(L, Key, Value).
+lookup_map(node(_, K, _, R), Key, Value) :-
+  Key > K,
+  !,
+  lookup_map(R, Key, Value).
+
+update_map(nil, Key, Value, node(nil, Key, Value, nil)).
+update_map(node(L, K, _, R), K, Value, node(L, K, Value, R)).
+update_map(node(L, K, V, R), Key, Value, node(NewL, K, V, R)) :-
+  Key < K,
+  !,
+  update_map(L, Key, Value, NewL).
+update_map(node(L, K, V, R), Key, Value, node(L, K, V, NewR)) :-
+  Key > K,
+  !,
+  update_map(R, Key, Value, NewR).
+
+init_arrays([], []).
+init_arrays([H|T], [(H,map(nil))|AT]) :-
+  init_arrays(T, AT). 
+
+init_procs(N, PrList) :-
+  length(PrList, N),
+  maplist(=(1), PrList).
+
+                                % initState(+Program, +N, -StanPoczatkowy)
+initState((vars(V), arrays(A), program(_)), N, (Vars, Arrs, PrList)) :-
+  init_vars(V, Vars),
+  init_arrays(A, Arrs),
+  init_procs(N, PrList).
+
+
+illegalState(program(P), (_, _, PrL)) :-
+  nth0(Pr1, PrL, CtPr1),
+  nth0(Pr2, PrL, CtPr2),
+  Pr1 =\= Pr2,
+  nth1(CtPr1, P, sekcja),
+  nth1(CtPr2, P, sekcja),
+  !,
+  format('Procesy w sekcji: ~p, ~p.~n', [Pr1, Pr2]).
+
+update_list([_|T], 0, Val, [Val|T]).
+update_list([H|T], N, Val, [H|NT]) :-
+  M is N - 1,
+  update_list(T, M, Val, NT).
+
+find_arr([(X, Tab)|_], X, Tab).
+find_arr([_|T], X, Tab) :-
+  find_arr(T, X, Tab).
+
+lookup_var([(X, Val)|_], X, Val).
+lookup_var([_|T], X, Val) :-
+  lookup_var(T, X, Val).
+
+oblicz(pid, (PrId, _, _), PrId) :- !.
+oblicz(N, (_, _, _), N) :-
+  number(N),
+  !.
+oblicz(arr(X, Wyr), (PrId, Var, Arr), Val) :-
+  atomic(X),
+  oblicz(Wyr, (PrId, Var, Arr), Idx),
+  find_arr(Arr, X, Tab),
+  lookup_map(Tab, Idx, Val),
+  !.
+oblicz(X, (_, Var, _), Val) :-
+  atomic(X),
+  lookup_var(Var, X, Val),
+  !.
+oblicz(Comp, (PrId, Var, Arr), Val) :-
+  functor(Comp, _, 2),
+  Comp =.. [Op, Wyr1, Wyr2],
+  oblicz(Wyr1, (PrId, Var, Arr), Val1),
+  oblicz(Wyr2, (PrId, Var, Arr), Val2),
+  RetComp =.. [Op, Val1, Val2],
+  !,
+  Val is RetComp.
+
+
+wykonaj(sekcja, PrId, CtrPr, (Var, Arr, PrL), (Var, Arr, PrLWy)) :-
+  Val is CtrPr + 1,
+  update_list(PrL, PrId, Val, PrLWy).
+wykonaj(goto(N), PrId, _, (Var, Arr, PrL), (Var, Arr, PrLWy)) :-
+  update_list(PrL, PrId, N, PrLWy).
+wykonaj(assign(X, Wyr), PrId, CtrPr, (Var, Arr, PrL), (VarWy, ArrWy, PrLWy)) :-
+  CtrPtrWy is CtrPr + 1,
+  update_list(PrL, PrId, CtrPtrWy, PrLWy),
+  oblicz(Wyr, (PrId, Var, Arr), Val),
+  przypisz(X, Val, (Var, Arr), (VarWy, ArrWy)).
+  
+
+                                % step(+Program, +StanWe, -PrId, -StanWy)
+step((_, _, program(P)), (Var, Arr, PrL), PrId, (VarWy, ArrWy, PrLWy)) :-
+  nth0(PrId, PrL, CtrPr),
+  nth1(CtrPr, P, Ins),
+  wykonaj(Ins, PrId, CtrPr, (Var, Arr, PrL), (VarWy, ArrWy, PrLWy)).
+
+verify(N, _) :-
+  N =< 0,
+  format('Error: Niepoprawna wartosc N - ~p.~n', N).
+verify(N, Program) :-
+  set_prolog_flag(fileerrors, off),
+  see(Program),
+  !,
+  read(V),
+  read(A),
+  read(P),
+  seen,
+  initState((V, A, P), N, StanP),
+  write(StanP).
+verify(_, Program) :-
+  format('Error: niepoprawna nazwa pliku - ~p.~n', [Program]).
+                                % verify(N, Program)
+                                % initState(+Program, +N, -StanPoczatkowy)
+                                % step(+Program, +StanWe, ?PrId, -StanWy)
